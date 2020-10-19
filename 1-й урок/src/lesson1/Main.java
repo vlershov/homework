a260a5e474a5190129cdc2a9516012b442fb8e68
vlershov <vlershov@mail.ru>
2020-10-19 23:10:25 +0300

    Creat repo

---
+++ 1-й урок/src/lesson1/Main.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int integerValue = 2020;
+        long longValue = 100000000;
+        byte byteValue = -127;
+        short shortValue = -32000;
+
+        double doubleValue = 3.5d;
+        float floatValue = 2.4f;
+
+        char ch1 = '5';
+        String stringValue = "fdsfs";
+        int summa = 12;
+       // System.out.println(summa++);
+        System.out.println(++summa);
+        System.out.println(++summa);
+    }
+
+}
